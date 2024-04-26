ce15c45c58202a5823d0b4ab2e6b65db46746fb9
samuelbagin1 <samuel.bagin1@gmail.com>
2024-04-26 22:01:18 +0200

    edited height and color

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
 function App() {
   return (
-    <div className="bg-[#776C5F] text-[#FEFEFA] relative z-10 w-full h-screen">
-      
+    <div className="bg-zinc-300 text-zinc-900 relative w-screen h-screen justify-center items-center flex">
+        <div className='grid justify-items-center gap-1'>
+          <div className='md:text-lg'>website under development</div>
+          <img src={Logo} alt="" className='md:w-[120px] w-[90px] ' />
+        </div>
     </div>
   );
 }
