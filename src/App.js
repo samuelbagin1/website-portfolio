7d323f63bdabaf23e9d2fae4b44e4115b0b2080e
samuelbagin1 <samuel.bagin1@gmail.com>
2024-04-27 12:27:24 +0200

    changed background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Logo from './assets/sign samuelbagin-black.png'
+import Logo from './assets/sign samuelbagin-white.png'
+import backImage from './assets/umbra-11.jpeg'
 
 function App() {
   return (
-    <div className="bg-[#FEFEFA] text-zinc-900 relative w-screen h-dvh justify-center items-center flex">
-        <div className='grid justify-items-center gap-1'>
-          <div className='md:text-lg'>website under development</div>
+    <div className="bg-[#FEFEFA] text-[#FEFEFA] relative w-screen h-dvh justify-center items-center flex">
+        <img src={backImage} className='absolute h-screen object-cover z-0 w-full' />
+        <div className='grid justify-items-center gap-1 z-10'>
+          <div className='md:text-lg text-sm'>website under development</div>
         </div>
-        <img src={Logo} alt="" className='md:w-[120px] w-[90px] absolute bottom-3' />
+        <img src={Logo} alt="" className='md:w-[120px] w-[60px] absolute bottom-3 z-10 opacity-80' />
     </div>
   );
 }
