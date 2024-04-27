48161e90d177d719fca8251f9bb89e71813bcc7b
samuelbagin1 <samuel.bagin1@gmail.com>
2024-04-27 12:29:15 +0200

    backImage

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import backImage from './assets/umbra-11.jpeg'
 function App() {
   return (
     <div className="bg-[#FEFEFA] text-[#FEFEFA] relative w-screen h-dvh justify-center items-center flex">
-        <img src={backImage} className='absolute h-screen object-cover z-0 w-full' />
+        <img src={backImage} alt="image at the back" className='absolute h-screen object-cover z-0 w-full' />
         <div className='grid justify-items-center gap-1 z-10'>
           <div className='md:text-lg text-sm'>website under development</div>
         </div>
-        <img src={Logo} alt="" className='md:w-[120px] w-[60px] absolute bottom-3 z-10 opacity-80' />
+        <img src={Logo} alt="aaa" className='md:w-[120px] w-[60px] absolute bottom-3 z-10 opacity-80' />
     </div>
   );
 }
