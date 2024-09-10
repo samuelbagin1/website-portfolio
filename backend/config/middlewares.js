29cb2b63540c946da9bf1dbdfa42345bfaed4bec
samuelbagin1 <samuel.bagin1@gmail.com>
2024-09-10 19:26:49 +0200

    middle

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -17,7 +17,7 @@ module.exports = ({ env }) => ({
   settings: {
     cors: {
       enabled: true,
-      origin: env('CORS_ORIGIN', 'http://localhost:3000, http://head.samuelbagin.xyz').split(',').map(origin => origin.trim()),
+      origin: env('CORS_ORIGIN', 'http://localhost:3000, http://website-portfolio-head-1o44bift8-samuelbagins-projects.vercel.app').split(',').map(origin => origin.trim()),
       headers: ['*'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
     },
