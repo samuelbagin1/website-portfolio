01d5bfa5174a2cc379e236cd71b95d65af036c33
samuelbagin1 <samuel.bagin1@gmail.com>
2024-09-10 19:40:10 +0200

    done

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,25 +1,34 @@
 // Path: ./config/middleware.js
-module.exports = ({ env }) => ({
-  load: {
-    before: [
-      'strapi::logger',
-      'strapi::errors',
-      'strapi::security',
-      'strapi::cors',
-      'strapi::poweredBy',
-      'strapi::query',
-      'strapi::body',
-      'strapi::session',
-      'strapi::favicon',
-      'strapi::public',
-    ],
-  },
-  settings: {
-    cors: {
-      enabled: true,
-      origin: env('CORS_ORIGIN', 'http://localhost:3000, http://website-portfolio-head-1o44bift8-samuelbagins-projects.vercel.app').split(',').map(origin => origin.trim()),
-      headers: ['*'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+module.exports = ({ env }) => [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
     },
   },
-});
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: env('CORS_ORIGIN', 'http://localhost:3000, https://website-portfolio-head.vercel.app').split(',').map(origin => origin.trim()),
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
