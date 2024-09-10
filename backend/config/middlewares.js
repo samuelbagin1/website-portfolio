8eb8146a7253eb4e71fa1bf351e0dbf1634bf28a
samuelbagin1 <samuel.bagin1@gmail.com>
2024-09-10 19:19:20 +0200

    middlewares

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,25 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+// Path: ./config/middleware.js
+module.exports = ({ env }) => ({
+  load: {
+    before: [
+      'strapi::logger',
+      'strapi::errors',
+      'strapi::security',
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::query',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::public',
+    ],
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: env('CORS_ORIGIN', 'http://localhost:3000, http://head.samuelbagin.xyz').split(',').map(origin => origin.trim()),
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    },
+  },
+});
