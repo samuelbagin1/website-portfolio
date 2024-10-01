18eb550f18db81e129bd2cc19c417aeac639f0db
samuelbagin1 <samuel.bagin1@gmail.com>
2024-10-01 13:40:23 +0200

    changed bg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import backImage from "./assets/background.gif";
+import backImage from "./assets/background.GIF";
 import { Link } from "react-router-dom"
 
 
