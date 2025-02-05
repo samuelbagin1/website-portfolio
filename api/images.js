473ae54750f7dab159dfce21c45e39526ed563e8
samuelbagin1 <samuel.bagin1@gmail.com>
2025-02-05 21:13:32 +0100

    mmmmmm

--- api/images.js
+++ api/images.js
@@ -1,4 +1,5 @@
-import { connectToDatabase } from '../../lib/mongodb';
+// Correct import path
+import { connectToDatabase } from '../lib/mongodb';
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
