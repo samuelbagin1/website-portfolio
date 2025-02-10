3d54c278a461baba3b4caa844e76805a3d5f8f67
samuelbagin1 <samuel.bagin1@gmail.com>
2025-02-10 22:33:37 +0100

    sklfnjsk

--- src/components/ImageUploadForm.jsx
+++ src/components/ImageUploadForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://head.samuelbagin.xyz/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 const ImageUploadForm = ({ onUploadSuccess }) => {
   const [text, setText] = useState('');
